357b7616fb39160647ce86d900a87b7461223f30
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2023-07-09 12:25:49 +0300

    Updating the code

--- Programming Basics with Python/PB - Exams/2016_april_24/2. the_sleeping_cat_tom.py
+++ Programming Basics with Python/PB - Exams/2016_april_24/2. the_sleeping_cat_tom.py
@@ -9,16 +9,13 @@ playing_while_working = work_days * playing_while_working_minutes
 playing_while_resting = num_rest_days * playing_while_resting_minutes
 play_time = playing_while_working + playing_while_resting
 
-minutes_for_sleeping_left = playing_norm_minutes_per_year - play_time
+minutes_difference = abs(playing_norm_minutes_per_year - play_time)
+only_hours = minutes_difference // 60
+only_minutes = minutes_difference % 60 # % 60 - взима само остатъка, като така остават минутите;
 
 if play_time > playing_norm_minutes_per_year:
-    more_minutes_playing = play_time - playing_norm_minutes_per_year
-    only_hours_playing = more_minutes_playing // 60
-    only_minutes_playing = more_minutes_playing % 60 # % 60 - взима само остатъка, като така остават минутите;
     print("Tom will run away")
-    print(f"{only_hours_playing} hours and {only_minutes_playing} minutes more for play")
+    print(f"{only_hours} hours and {only_minutes} minutes more for play")
 else:
-    only_hours_for_sleeping = minutes_for_sleeping_left // 60
-    only_minutes_for_sleeping = minutes_for_sleeping_left % 60 # % 60 - взима само остатъка, като така остават минутите;
     print("Tom sleeps well")
-    print(f"{only_hours_for_sleeping} hours and {only_minutes_for_sleeping} minutes less for play")
+    print(f"{only_hours} hours and {only_minutes} minutes less for play")
