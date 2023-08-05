0654f80edbac997452bc3c9f6e44b543a5ea2da0
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2023-08-05 14:59:53 +0300

    Updating code

--- Programming Basics with Python/11. Nested Loops/06_building.py
+++ Programming Basics with Python/11. Nested Loops/06_building.py
@@ -1,32 +1,15 @@
 floors_num = int(input())
 apartments_total = int(input())
 
-office_num = 0
-apartment_num = 0
+room_letter = ""
 
 for floor in range(floors_num, 0, -1):
     if floor == floors_num or floors_num == 1:
-        for i in range(apartments_total):
-            if i == apartments_total - 1:
-                print(f"L{floor}{apartment_num}", end="")
-            else:
-                print(f"L{floor}{apartment_num} ", end="")
-            apartment_num += 1
-        apartment_num = 0
+        room_letter = "L"
     elif floor % 2 == 0:
-        for i in range(apartments_total):
-            if i == apartments_total - 1:
-                print(f"O{floor}{office_num}", end="")
-            else:
-                print(f"O{floor}{office_num} ", end="")
-            office_num += 1
-        office_num = 0
+        room_letter = "O"
     else:
-        for i in range(apartments_total):
-            if i == apartments_total - 1:
-                print(f"A{floor}{apartment_num}", end="")
-            else:
-                print(f"A{floor}{apartment_num} ", end="")
-            apartment_num += 1
-        apartment_num = 0
+       room_letter = "A"
+    for room in range(apartments_total):
+        print(f"{room_letter}{floor}{room}", end=" ")
     print()
