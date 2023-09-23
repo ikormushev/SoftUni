c85cd051c6816d34d1de6a7aa45a3833928b5622
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2023-09-23 22:35:46 +0300

    Update 07_water_overflow.py

--- Programming Fundamentals with Python/02.1 Data Types and Variables - Exercise/07_water_overflow.py
+++ Programming Fundamentals with Python/02.1 Data Types and Variables - Exercise/07_water_overflow.py
@@ -1,12 +1,13 @@
 lines_num = int(input())
 
 tank_capacity = 255
+tank_capacity_left = tank_capacity
 
 for _ in range(lines_num):
     water = int(input())
-    if water > tank_capacity:
+    if water > tank_capacity_left:
         print("Insufficient capacity!")
         continue
-    tank_capacity -= water
+    tank_capacity_left -= water
 
-print(255 - tank_capacity)
+print(tank_capacity - tank_capacity_left)
