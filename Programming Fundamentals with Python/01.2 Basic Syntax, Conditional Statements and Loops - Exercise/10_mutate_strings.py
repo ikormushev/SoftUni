a2c7b0fbf8ba075c5a33f249f77f5f1dd0b2bebc
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2023-09-23 20:33:35 +0300

    Delete Programming Fundamentals with Python/01.2 Basic Syntax, Conditional Statements and Loops - Exercise directory

--- Programming Fundamentals with Python/01.2 Basic Syntax, Conditional Statements and Loops - Exercise/10_mutate_strings.py
+++
@@ -1,9 +0,0 @@
-first_word = input()
-second_word = input()
-
-first_word_list = list(first_word)
-
-for i in range(len(first_word_list)):
-    if first_word_list[i] != second_word[i]:
-        first_word_list[i] = second_word[i]
-        print("".join(first_word_list))
