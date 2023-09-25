279e1ce4baaadf1a4029944571128b038d661dac
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2023-09-26 01:23:47 +0300

    Create 01_strange_zoo.py

---
+++ Programming Fundamentals with Python/03. Lists Basics/01_strange_zoo.py
@@ -0,0 +1,7 @@
+tail = input()
+body = input()
+head = input()
+
+animal_unordered = [tail, body, head]
+animal_unordered.reverse()  # or animal_unordered.sort(reverse = True)
+print(animal_unordered)
