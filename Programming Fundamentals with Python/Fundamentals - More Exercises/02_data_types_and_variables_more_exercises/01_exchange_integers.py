3161de6c0f40498e45dd5902941b4a150418d4c4
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2023-10-01 20:19:35 +0300

    Delete Programming Fundamentals with Python/Fundamentals - More Exercises directory

--- Programming Fundamentals with Python/Fundamentals - More Exercises/02_data_types_and_variables_more_exercises/01_exchange_integers.py
+++
@@ -1,9 +0,0 @@
-first_number = int(input())
-second_number = int(input())
-
-print("Before:")
-print(f"a = {first_number}")
-print(f"b = {second_number}")
-print("After:")
-print(f"a = {second_number}")
-print(f"b = {first_number}")
--- Programming Fundamentals with Python/Fundamentals - More Exercises/02_data_types_and_variables_more_exercises/03_decrypting_messages.py
+++
@@ -1,9 +0,0 @@
-key = int(input())
-lines_num = int(input())
-
-word_list = []
-for i in range(1, lines_num + 1):
-    letter = input()
-    word_list.append(chr(ord(letter) + key))
-
-print("".join(word_list))
