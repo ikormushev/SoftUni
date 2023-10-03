b70aecc5e2e09d3297eaf7f0393fadc42585c2a1
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2023-10-03 10:14:24 +0300

    Update 03_football_cards.py

--- Programming Fundamentals with Python/03.1 Lists Basics - Exercise/03_football_cards.py
+++ Programming Fundamentals with Python/03.1 Lists Basics - Exercise/03_football_cards.py
@@ -4,16 +4,13 @@ cards_list = list(cards)
 
 first_team_players_count = 11
 second_team_players_count = 11
-team_loss = False
+terminated_game = False
 seen_players = []
 
 for i in range(len(cards_list)):
     if cards_list[i] in seen_players:
         continue
     seen_players.append(cards_list[i])
-    if first_team_players_count < 7 or second_team_players_count < 7:
-        team_loss = True
-        break
 
     if "A" in cards_list[i]:
         first_team_players_count -= 1
@@ -21,10 +18,10 @@ for i in range(len(cards_list)):
         second_team_players_count -= 1
 
     if first_team_players_count < 7 or second_team_players_count < 7:
-        team_loss = True
+        terminated_game = True
         break
 
 print(f"Team A - {first_team_players_count}; "
       f"Team B - {second_team_players_count}")
-if team_loss:
+if terminated_game:
     print("Game was terminated")
