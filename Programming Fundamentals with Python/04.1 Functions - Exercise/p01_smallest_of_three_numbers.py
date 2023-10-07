b62f535911f9c21372e58dd519d4e02abdb4eba3
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2023-10-07 22:44:21 +0300

    Create p01_smallest_of_three_numbers.py

---
+++ Programming Fundamentals with Python/04.1 Functions - Exercise/p01_smallest_of_three_numbers.py
@@ -0,0 +1,10 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+
+def smallest_number(x, y, z):
+    return min(x, y, z)
+
+
+print(smallest_number(first_number, second_number, third_number))
