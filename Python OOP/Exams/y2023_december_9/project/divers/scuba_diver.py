a344c0fda0b6a3cda6e8a7e220c79a40366a16b2
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2024-03-31 00:11:21 +0200

    Add files via upload

---
+++ Python OOP/Exams/y2023_december_9/project/divers/scuba_diver.py
@@ -0,0 +1,20 @@
+from project.divers.base_diver import BaseDiver
+
+
+class ScubaDiver(BaseDiver):
+    BASE_OXYGEN_LEVEL = 540
+    MISS_DECREASE_VALUE = 0.3
+
+    def __init__(self, name: str):
+        super().__init__(name, self.BASE_OXYGEN_LEVEL)
+
+    def miss(self, time_to_catch: int):
+        decrease_value = self.MISS_DECREASE_VALUE * time_to_catch
+        if self.oxygen_level - decrease_value < 0:
+            self.oxygen_level = 0
+        else:
+            self.oxygen_level -= decrease_value
+            self.oxygen_level = round(self.oxygen_level)
+
+    def renew_oxy(self):
+        self.oxygen_level = self.BASE_OXYGEN_LEVEL
