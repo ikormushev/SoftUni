3296c65d813fda04fd7510cc0452c4e2fe6fdd07
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2024-06-05 14:57:55 +0300

    Create section_one.sql

---
+++ PostgreSQL/Exam Preparations/Bio Bakery/section_one.sql
@@ -0,0 +1,92 @@
+-- Section 1
+CREATE TABLE countries(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE customers(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender CHAR(1) CHECK(gender = 'M' OR gender = 'F'),
+    age INT NOT NULL CHECK(age > 0),
+    phone_number CHAR(10) NOT NULL,
+    country_id INT NOT NULL,
+
+    CONSTRAINT fk_customers_countries
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE products(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(25) NOT NULL,
+    description VARCHAR(250),
+    recipe TEXT,
+    price NUMERIC(10, 2) NOT NULL CHECK(price > 0)
+);
+
+CREATE TABLE feedbacks(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    description VARCHAR(255),
+    rate NUMERIC(4, 2) CHECK(rate >= 0 AND rate <= 10),
+    product_id INT NOT NULL,
+    customer_id INT NOT NULL,
+
+    CONSTRAINT fk_feedbacks_products
+    FOREIGN KEY (product_id) REFERENCES products(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+    CONSTRAINT fk_feedbacks_customers
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE distributors(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(25) NOT NULL UNIQUE,
+    address VARCHAR(30) NOT NULL,
+    summary VARCHAR(200) NOT NULL,
+    country_id INT NOT NULL,
+
+    CONSTRAINT fk_distributors_countries
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE ingredients(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    description VARCHAR(200),
+    country_id INT NOT NULL,
+    distributor_id INT NOT NULL,
+
+    CONSTRAINT fk_ingredients_countries
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+    CONSTRAINT fk_ingredients_distributors
+    FOREIGN KEY (distributor_id) REFERENCES distributors(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE products_ingredients(
+    product_id INT,
+    ingredient_id INT,
+
+    CONSTRAINT fk_products_ingredients_products
+    FOREIGN KEY (product_id) REFERENCES products(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+    CONSTRAINT fk_products_ingredients_ingredients
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
