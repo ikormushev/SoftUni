14c0381ab4c440a58aacbc56c378a4128b8413f8
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2024-07-18 21:24:29 +0300

    Update models.py

--- Python ORM/07_Advanced_Django_Model_Techniques/EXERCISE/main_app/models.py
+++ Python ORM/07_Advanced_Django_Model_Techniques/EXERCISE/main_app/models.py
@@ -147,3 +147,6 @@ class Document(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     search_vector = SearchVectorField(null=True)
+
+    class Meta:
+        indexes=[models.Index(fields=["search_vector"])]
