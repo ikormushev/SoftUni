28bd695f72d2a7b264b459f0e4290de4ec5db290
Ivan Kormushev <138397840+ikormushev@users.noreply.github.com>
2024-08-17 19:52:44 +0300

    Wrong app.js

--- JS Front-End/08.Asynchronous-Programming/EXERCISE/01.Blog/app.js
+++ JS Front-End/08.Asynchronous-Programming/EXERCISE/01.Blog/app.js
@@ -1,29 +1,49 @@
 function attachEvents() {
-    const requestUrl = "http://localhost:3030/jsonstore/messenger";
+    const postsUrl = 'http://localhost:3030/jsonstore/blog/posts';
+    const commentsUrl = 'http://localhost:3030/jsonstore/blog/comments';
+    const loadButton = document.getElementById('btnLoadPosts');
+    const selectMenu = document.getElementById('posts');
+    const viewButton = document.getElementById('btnViewPost');
 
-    const messagesArea = document.getElementById('messages');
-    const sendButton = document.getElementById('submit');
-    const refreshButton = document.getElementById('refresh');
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const commentsUl = document.getElementById('post-comments');
 
-    const authorEl = document.querySelector('input[type="text"][name="author"]');
-    const contentEl = document.querySelector('input[type="text"][name="content"]');
+    let totalPosts = [];
 
-    sendButton.addEventListener('click', async () => {
-        await fetch(requestUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({author: authorEl.value, content: contentEl.value}),
-        })
+    loadButton.addEventListener('click', async () => {
+        selectMenu.innerHTML = "";
+        let response = await fetch(postsUrl);
+        let data = await response.json();
+
+        let postsData = Object.values(data);
+        postsData.forEach(post => {
+            let newOption = document.createElement('option');
+            newOption.value = post.id;
+            newOption.textContent = post.title;
+            totalPosts.push({id: post.id, title: post.title, body: post.body});
+        
+            selectMenu.appendChild(newOption);
+        });
+        
     });
 
-    refreshButton.addEventListener('click', async () => {
-        let response = await fetch(requestUrl);
+    viewButton.addEventListener('click', async () => {
+        commentsUl.innerHTML = "";
+        let response = await fetch(commentsUrl);
         let data = await response.json();
-        let messages = Object.values(data);
 
-        messagesArea.value = messages.map(message => `${message.author}: ${message.content}`).join("\n");
+        let commentsData = Object.values(data);
+        let rightPostComments = commentsData.filter(comment => comment.postId == selectMenu.value);
+        let rightPost = totalPosts.find(post => post.id === selectMenu.value);
+
+        postTitle.textContent = rightPost.title;
+        postBody.textContent = rightPost.body;
+        for (let comment of rightPostComments) {
+            let newLi = document.createElement('li');
+            newLi.textContent = comment.text;
+            commentsUl.appendChild(newLi);
+        }
     });
 }
 
